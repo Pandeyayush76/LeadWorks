e2f822bd0691bca31080f87190015c25b0939707
pande <pande@LAPTOP-7QPC96OE>
2022-07-18 21:24:22 +0530

    ok

--- src/test/java/GitHUbPracticePart1/PracticeAsaLead.java
+++ src/test/java/GitHUbPracticePart1/PracticeAsaLead.java
@@ -4,6 +4,7 @@ public class PracticeAsaLead {
 
 	public static void main(String[] args) {
 		System.out.println("Raja");
+		System.out.println("King of Amritsar");
 		
 
 	}
--- src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
+++ src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
@@ -6,6 +6,7 @@ public class PracticeAsaLead2 {
 System.out.println("Nikhil do everything");
 System.out.println("Nikhil when u will marry");
 System.out.println("Nikhil should i search girl for u");
+System.out.println("Girl should be on which state");
 
 	}
 
