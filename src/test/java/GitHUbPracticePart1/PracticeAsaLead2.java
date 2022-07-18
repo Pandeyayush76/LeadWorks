9aad48e21b4fcedc7e0af87d750ac5be9db98297
NIKHIL RAJ <NIKHIL RAJ@DESKTOP-J9EO0C6>
2022-07-18 21:16:02 +0530

    change done by nikhil

--- src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
+++ src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
@@ -6,6 +6,9 @@ public class PracticeAsaLead2 {
 System.out.println("Nikhil do everything");
 System.out.println("Nikhil when u will marry");
 System.out.println("Nikhil should i search girl for u");
+System.out.println("yes");
+System.out.println("next year");
+System.out.println("yes");
 
 	}
 
