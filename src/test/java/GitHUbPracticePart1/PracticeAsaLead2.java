f028825542f65d37cc6938243c7b0b02a07a27d9
pande <pande@LAPTOP-7QPC96OE>
2022-07-18 20:55:19 +0530

    do in this

--- src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
+++ src/test/java/GitHUbPracticePart1/PracticeAsaLead2.java
@@ -4,6 +4,8 @@ public class PracticeAsaLead2 {
 
 	public static void main(String[] args) {
 System.out.println("Nikhil do everything");
+System.out.println("Nikhil when u will marry");
+System.out.println("Nikhil should i search girl for u");
 
 	}
 
